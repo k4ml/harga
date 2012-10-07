5e064433c8c9ce091ed7f21386f479b0374ef9f5
Kamal <kamal.mustafa@gmail.com>
2012-10-07 12:45:59 -0500

    reenable index_queryset

--- harga/search_indexes.py
+++ harga/search_indexes.py
@@ -8,9 +8,9 @@ class ProductIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
     tarikh = DateField(model_attr='tarikh_iso')
 
-    def index_querysetx(self):
+    def index_queryset(self):
         """Used when the entire index for model is updated."""
-        return Product.objects.filter(tarikh__lte=datetime.datetime.now().date())
+        return Product.objects.filter(modified__lte=datetime.datetime.now())
 
 
 site.register(Product, ProductIndex)
