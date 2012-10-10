3259d50d2963aace2bb2a952771a4ec9c5cf7ea3
Kamal <kamal.mustafa@gmail.com>
2012-10-10 04:18:16 -0500

    change admin widget for projectextra

--- harga/admin.py
+++ harga/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
+from django import forms
 
 from harga.models import Product, ProductExtra
 
 class ProductAdmin(admin.ModelAdmin):
     pass
 
+class ProductExtraAdminForm(forms.ModelForm):
+    class Meta:
+        model = ProductExtra
+        widgets = {
+            'tarikh': forms.TextInput,
+            'nama': forms.TextInput(attrs={'size': '80'}),
+            'premis': forms.TextInput(attrs={'size': '80'}),
+            'harga': forms.TextInput,
+            'kawasan': forms.TextInput(attrs={'size': '80'}),
+        }
+
 class ProductExtraAdmin(admin.ModelAdmin):
-    pass
+    form = ProductExtraAdminForm
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductExtra, ProductExtraAdmin)
